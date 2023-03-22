5264e6a90d4e92ab4ad9b62bf662327d43d0b65b
Daniella <dansmc.courses@gmail.com>
2023-03-22 17:00:45 -0600

    live coding 2 y retroalimentacion

--- starter-files/script.js
+++ starter-files/script.js
@@ -11,3 +11,25 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+const button = document.getElementById('button');
+const answer = document.getElementById('answer');
+const input = document.getElementById('input');
+
+const fetchAnswer = () =>{
+    fetch(API_ENDPOINT)
+    .then((data) => data.json())
+    .then(data => {
+        answer.innerText = (input.value == "")? "":data.answer;
+        setTimeout(() => {
+        answer.innerText = "";
+}, 5000);
+    })
+    .catch((err) => {
+        
+    });
+}
+
+button.addEventListener('click',fetchAnswer)
+
+
+
